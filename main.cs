0d3909560f661f8959d95cf678b92ce97d79c22e
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-30 01:01:35 +0000

    stuff

--- main.cs
+++ main.cs
@@ -16,16 +16,16 @@ class Program
 
         Console.Write("Enter length a: ");
         a = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter length b ");
+        Console.Write("Enter length b: ");
         b = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter length c ");
+        Console.Write("Enter length c: ");
         c = Convert.ToInt32(Console.ReadLine());
 
-        int angleA = Math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * (180/Math.PI)
-        int angleB = Math.acos((c**2 + a**2 - b**2) / (2 * c * a)) * (180/Math.PI)
-        int angleC = Math.acos((a**2 + b**2 - c**2) / (2 * a * b)) * (180/Math.PI)
-
-        int sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+        double angleA = Math.Acos((Math.Pow(b,2) + Math.Pow(c,2) - Math.Pow(a,2)) / (2 * b * c)) * (180/Math.PI);
+        double angleB = Math.Acos((Math.Pow(c,2) + Math.Pow(a,2) - Math.Pow(b,2)) / (2 * c * a)) * (180/Math.PI);
+        double angleC = Math.Acos((Math.Pow(a,2) + Math.Pow(b,2) - Math.Pow(c,2)) / (2 * a * b)) * (180/Math.PI);
+            
+        double sumOfAngles = (Math.Round(angleA,2)) + (Math.Round(angleB,2)) + (Math.Round(angleC,2));
 
         if (sumOfAngles == 180)
         {
